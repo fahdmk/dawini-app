459837d12cf8445b1ce3f166f7d063da49490bb1
Thomas-Max99 <thomassentre332@gmail.com>
2023-06-01 00:09:59 +0000

    adding sportswear shop

--- settings-profile/screens/Profile.js
+++ settings-profile/screens/Profile.js
@@ -1,89 +1,98 @@
-import { View, Text, Image, TouchableOpacity, useWindowDimensions, FlatList } from 'react-native'
-import React, { useState } from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  useWindowDimensions,
+  FlatList,
+} from "react-native";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { COLORS, FONTS, SIZES, images } from "../constants";
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import { MaterialIcons } from "@expo/vector-icons";
-import { SceneMap, TabBar, TabView } from 'react-native-tab-view';
-import { photos } from "../constants/data"
+import { SceneMap, TabBar, TabView } from "react-native-tab-view";
+import { photos } from "../constants/data";
 
-const PhotosRoutes = ()=>(
-  <View style={{flex: 1}}>
+const PhotosRoutes = () => (
+  <View style={{ flex: 1 }}>
     <FlatList
       data={photos}
       numColumns={3}
-      renderItem={({item,index}) =>(
-        <View style={{
-          flex: 1,
-          aspectRatio: 1,
-          margin: 3
-        }}>
+      renderItem={({ item, index }) => (
+        <View
+          style={{
+            flex: 1,
+            aspectRatio: 1,
+            margin: 3,
+          }}
+        >
           <Image
             key={index}
             source={item}
-            style={{width: "100%", height: "100%", borderRadius: 12}}
+            style={{ width: "100%", height: "100%", borderRadius: 12 }}
           />
         </View>
       )}
     />
   </View>
-)
+);
 
-const LikesRoutes = ()=>(
+const LikesRoutes = () => (
   <View
     style={{
       flex: 1,
-      backgroundColor: "blue"
+      backgroundColor: "blue",
     }}
   />
-)
+);
 
 const renderScene = SceneMap({
   first: PhotosRoutes,
-  second: LikesRoutes
+  second: LikesRoutes,
 });
 
-
 const Profile = () => {
-
   const layout = useWindowDimensions();
-  const [index,setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
   const [routes] = useState([
-    {key: "first", title: "Photos"},
-    {key: "second", title: "Likes"}
-  ])
+    { key: "first", title: "Photos" },
+    { key: "second", title: "Likes" },
+  ]);
 
-  const renderTabBar = (props)=>(
+  const renderTabBar = (props) => (
     <TabBar
-      {...props} 
+      {...props}
       indicatorStyle={{
-        backgroundColor: COLORS.primary
+        backgroundColor: COLORS.primary,
       }}
       style={{
         backgroundColor: COLORS.white,
-        height: 44
+        height: 44,
       }}
-      renderLabel={({focused, route})=>(
-        <Text style={[{ color: focused ? COLORS.black : COLORS.gray}]}>
+      renderLabel={({ focused, route }) => (
+        <Text style={[{ color: focused ? COLORS.black : COLORS.gray }]}>
           {route.title}
         </Text>
       )}
     />
-  )
+  );
   return (
-    <SafeAreaView style={{
-      flex: 1,
-      backgroundColor: COLORS.white
-    }}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: COLORS.white,
+      }}
+    >
       <StatusBar backgroundColor={COLORS.gray} />
       <View style={{ width: "100%" }}>
         <Image
           source={images.cover}
-          resizeMode='cover'
+          resizeMode="cover"
           style={{
             height: 228,
-            width: "100%"
+            width: "100%",
           }}
         />
       </View>
@@ -91,99 +100,136 @@ const Profile = () => {
       <View style={{ flex: 1, alignItems: "center" }}>
         <Image
           source={images.profile}
-          resizeMode='contain'
+          resizeMode="contain"
           style={{
             height: 155,
             width: 155,
             borderRadius: 999,
             borderColor: COLORS.primary,
             borderWidth: 2,
-            marginTop: -90
+            marginTop: -90,
           }}
         />
 
-        <Text style={{
-          ...FONTS.h3,
-          color: COLORS.primary,
-          marginVertical: 8
-        }}>Melissa Peters</Text>
-        <Text style={{
-          color: COLORS.black,
-          ...FONTS.body4
-        }}>
+        <Text
+          style={{
+            ...FONTS.h3,
+            color: COLORS.primary,
+            marginVertical: 8,
+          }}
+        >
+          Melissa Peters
+        </Text>
+        <Text
+          style={{
+            color: COLORS.black,
+            ...FONTS.body4,
+          }}
+        >
           Interior designer
         </Text>
 
-        <View style={{
-          flexDirection: "row",
-          marginVertical: 6,
-          alignItems: "center"
-        }}>
-          <MaterialIcons
-            name="location-on"
-            size={24}
-            color="black"
-          />
-          <Text style={{
-            ...FONTS.body4,
-            marginLeft: 4
-          }}>Lagos, Nigeria</Text>
+        <View
+          style={{
+            flexDirection: "row",
+            marginVertical: 6,
+            alignItems: "center",
+          }}
+        >
+          <MaterialIcons name="location-on" size={24} color="black" />
+          <Text
+            style={{
+              ...FONTS.body4,
+              marginLeft: 4,
+            }}
+          >
+            Lagos, Nigeria
+          </Text>
         </View>
 
-        <View style={{
-          paddingVertical: 8,
-          flexDirection: "row"
-        }}>
-          <View style={{
-            flexDirection: "column",
-            alignItems: "center",
-            marginHorizontal: SIZES.padding
-          }}>
-            <Text style={{
-              ...FONTS.h2,
-              color: COLORS.primary
-            }}>122</Text>
-            <Text style={{
-              ...FONTS.body4,
-              color: COLORS.primary
-            }}>Followers</Text>
+        <View
+          style={{
+            paddingVertical: 8,
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              marginHorizontal: SIZES.padding,
+            }}
+          >
+            <Text
+              style={{
+                ...FONTS.h2,
+                color: COLORS.primary,
+              }}
+            >
+              122
+            </Text>
+            <Text
+              style={{
+                ...FONTS.body4,
+                color: COLORS.primary,
+              }}
+            >
+              Followers
+            </Text>
           </View>
 
-          <View style={{
-            flexDirection: "column",
-            alignItems: "center",
-            marginHorizontal: SIZES.padding
-          }}>
-            <Text style={{
-              ...FONTS.h2,
-              color: COLORS.primary
-            }}>67</Text>
-            <Text style={{
-              ...FONTS.body4,
-              color: COLORS.primary
-            }}>Followings</Text>
+          <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              marginHorizontal: SIZES.padding,
+            }}
+          >
+            <Text
+              style={{
+                ...FONTS.h2,
+                color: COLORS.primary,
+              }}
+            >
+              67
+            </Text>
+            <Text
+              style={{
+                ...FONTS.body4,
+                color: COLORS.primary,
+              }}
+            >
+              Followings
+            </Text>
           </View>
 
-          <View style={{
-            flexDirection: "column",
-            alignItems: "center",
-            marginHorizontal: SIZES.padding
-          }}>
-            <Text style={{
-              ...FONTS.h2,
-              color: COLORS.primary
-            }}>77K</Text>
-            <Text style={{
-              ...FONTS.body4,
-              color: COLORS.primary
-            }}>Likes</Text>
+          <View
+            style={{
+              flexDirection: "column",
+              alignItems: "center",
+              marginHorizontal: SIZES.padding,
+            }}
+          >
+            <Text
+              style={{
+                ...FONTS.h2,
+                color: COLORS.primary,
+              }}
+            >
+              77K
+            </Text>
+            <Text
+              style={{
+                ...FONTS.body4,
+                color: COLORS.primary,
+              }}
+            >
+              Likes
+            </Text>
           </View>
-
-
         </View>
 
         <View style={{ flexDirection: "row" }}>
-
           <TouchableOpacity
             style={{
               width: 124,
@@ -192,14 +238,17 @@ const Profile = () => {
               justifyContent: "center",
               backgroundColor: COLORS.primary,
               borderRadius: 10,
-              marginHorizontal: SIZES.padding * 2
+              marginHorizontal: SIZES.padding * 2,
             }}
           >
-            <Text style={{
-              ...FONTS.body4,
-              color: COLORS.white
-            }}>Edit Profile</Text>
-
+            <Text
+              style={{
+                ...FONTS.body4,
+                color: COLORS.white,
+              }}
+            >
+              Edit Profile
+            </Text>
           </TouchableOpacity>
 
           <TouchableOpacity
@@ -210,31 +259,32 @@ const Profile = () => {
               justifyContent: "center",
               backgroundColor: COLORS.primary,
               borderRadius: 10,
-              marginHorizontal: SIZES.padding * 2
+              marginHorizontal: SIZES.padding * 2,
             }}
           >
-            <Text style={{
-              ...FONTS.body4,
-              color: COLORS.white
-            }}>Add Friend</Text>
-
+            <Text
+              style={{
+                ...FONTS.body4,
+                color: COLORS.white,
+              }}
+            >
+              Add Friend
+            </Text>
           </TouchableOpacity>
-
         </View>
       </View>
 
-      <View style={{flex: 1, marginHorizontal: 22, marginTop: 20}}>
-            <TabView
-              navigationState={{ index, routes }}
-              renderScene={renderScene}
-              onIndexChange={setIndex}
-              initialLayout={{width: layout.width}}
-              renderTabBar={renderTabBar}
-            />
+      <View style={{ flex: 1, marginHorizontal: 22, marginTop: 20 }}>
+        <TabView
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={{ width: layout.width }}
+          renderTabBar={renderTabBar}
+        />
       </View>
-
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
---
+++ sportswear-shop-app/screens/index.js
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import Search from "./Search";
+import Favourite from "./Favourite";
+import Profile from "./Profile";
+import Details from "./Details";
+
+export {
+    Home,
+    Search,
+    Favourite,
+    Profile,
+    Details
+}
